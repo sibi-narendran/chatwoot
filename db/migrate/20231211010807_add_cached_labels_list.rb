3526e68ccbf6d3f31be09cfa681cf4f186d26814
sibi-narendran <sibinarendran@gmail.com>
2025-11-17 20:18:19 +0530

    from mac

--- db/migrate/20231211010807_add_cached_labels_list.rb
+++ db/migrate/20231211010807_add_cached_labels_list.rb
@@ -2,6 +2,10 @@ class AddCachedLabelsList < ActiveRecord::Migration[7.0]
   def change
     add_column :conversations, :cached_label_list, :string
     Conversation.reset_column_information
-    ActsAsTaggableOn::Taggable::Cache.included(Conversation)
+    if defined?(ActsAsTaggableOn::Taggable::Cache)
+      ActsAsTaggableOn::Taggable::Cache.included(Conversation)
+    else
+      say 'ActsAsTaggableOn::Taggable::Cache missing, skipping include', true
+    end
   end
 end
--- db/migrate/20250104200055_create_captain_tables.rb
+++ db/migrate/20250104200055_create_captain_tables.rb
@@ -1,12 +1,16 @@
 class CreateCaptainTables < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
   def up
-    # Post this migration, the 'vector' extension is mandatory to run the application.
-    # If the extension is not installed, the migration will raise an error.
-    setup_vector_extension
+    # Captain requires the pgvector extension for vector similarity.
+    # When the managed Postgres does not expose the extension, we still create the tables
+    # with JSONB-based embeddings so that the rest of the application can migrate.
+    vector_supported = setup_vector_extension
+    say "Proceeding without the 'vector' extension; Captain stays disabled. See https://chwt.app/v4/migration", true unless vector_supported
+
     create_assistants
     create_documents
-    create_assistant_responses
-    create_old_tables
+    create_assistant_responses(vector_supported:)
+    create_old_tables(vector_supported:)
   end
 
   def down
@@ -22,13 +26,13 @@ class CreateCaptainTables < ActiveRecord::Migration[7.0]
   private
 
   def setup_vector_extension
-    return if extension_enabled?('vector')
+    return true if extension_enabled?('vector')
 
-    begin
-      enable_extension 'vector'
-    rescue ActiveRecord::StatementInvalid
-      raise StandardError, "Failed to enable 'vector' extension. Read more at https://chwt.app/v4/migration"
-    end
+    enable_extension 'vector'
+    true
+  rescue ActiveRecord::StatementInvalid => e
+    say "Failed to enable 'vector' extension (#{e.message}).", true
+    false
   end
 
   def create_assistants
@@ -60,11 +64,15 @@ class CreateCaptainTables < ActiveRecord::Migration[7.0]
     add_index :captain_documents, [:assistant_id, :external_link], unique: true
   end
 
-  def create_assistant_responses
+  def create_assistant_responses(vector_supported:)
     create_table :captain_assistant_responses do |t|
       t.string :question, null: false
       t.text :answer, null: false
-      t.vector :embedding, limit: 1536
+      if vector_supported
+        t.vector :embedding, limit: 1536
+      else
+        t.jsonb :embedding, default: []
+      end
       t.bigint :assistant_id, null: false
       t.bigint :document_id
       t.bigint :account_id, null: false
@@ -75,16 +83,27 @@ class CreateCaptainTables < ActiveRecord::Migration[7.0]
     add_index :captain_assistant_responses, :account_id
     add_index :captain_assistant_responses, :assistant_id
     add_index :captain_assistant_responses, :document_id
-    add_index :captain_assistant_responses, :embedding, using: :ivfflat, name: 'vector_idx_knowledge_entries_embedding', opclass: :vector_l2_ops
+    if vector_supported
+      add_index :captain_assistant_responses, :embedding,
+                using: :ivfflat,
+                name: 'vector_idx_knowledge_entries_embedding',
+                opclass: :vector_l2_ops
+    end
   end
 
-  def create_old_tables
+  def create_old_tables(vector_supported:)
     create_table :article_embeddings, if_not_exists: true do |t|
       t.bigint :article_id, null: false
       t.text :term, null: false
-      t.vector :embedding, limit: 1536
+      if vector_supported
+        t.vector :embedding, limit: 1536
+      else
+        t.jsonb :embedding, default: []
+      end
       t.timestamps
     end
-    add_index :article_embeddings, :embedding, if_not_exists: true, using: :ivfflat, opclass: :vector_l2_ops
+    if vector_supported
+      add_index :article_embeddings, :embedding, if_not_exists: true, using: :ivfflat, opclass: :vector_l2_ops
+    end
   end
 end
